34cf0c90e527b35ee875978065e6aa1ab5fbfe00
Xuanz <152081122+xuanz-ai@users.noreply.github.com>
2026-03-24 03:31:34 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "localhost",
-    "127.0.0.1",
-    "192.168.152.239",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.152.239:3000",
-  ],
+  output: "standalone",
 };
 
 export default nextConfig;
